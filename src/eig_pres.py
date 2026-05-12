e5974b94b2169c5f05a31eb32f63e3e08d980425
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-12 14:11:24 -0600

    generating plots

--- src/eig_pres.py
+++ src/eig_pres.py
@@ -163,13 +163,13 @@ def run_scikit_eig_percent_reduce(plotter, mats, seed, num_avg, type="jl_gaussia
                  args={"type":"jl_sparse"})
 
 if __name__ == '__main__':
-    plotter = Plotter(save_fig=True, show_fig=True)
+    plotter = Plotter(save_fig=True, show_fig=True, fig_size=(12, 6))
     # mats    = ["494_bus"]
-    mats    = ["494_bus", "1138_bus", "bibd_11_5", "bibd_13_6",]
-    mats    = ["bcsstk07", "bcsstk19", "bcsstm07", "impcol_d"]
+    mats    = ["494_bus", "1138_bus", "bibd_11_5", "bibd_13_6","bcsstk08"]
+    # mats    = ["bcsstk07", "bcsstk19", "bcsstm07", "impcol_d"]
 
     seed    = 10
-    num_avg = 5
+    num_avg = 1
 
     run_scikit_eig_percent_reduce(plotter, mats, seed, num_avg, type="jl_gaussian")
     run_scikit_eig_percent_reduce(plotter, mats, seed, num_avg, type="jl_sparse")
