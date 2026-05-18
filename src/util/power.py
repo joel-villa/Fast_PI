16329eaec0d203f445b6dc2055ef91e28c869d23
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-18 11:44:27 -0600

    implemented convergence breakout

--- src/util/power.py
+++ src/util/power.py
@@ -25,24 +25,50 @@ def v_from_u(A, u):
     
     
 
-def topsing(v0, A, maxiter=10):
+def topsing(v0, A, maxiter=10, tolerance=1e-07):
     """
     v0      - an initial guess for the top right eigenvector (m-dimensional)
     A       - A matrix (nxm) s.t. n is less than or equal to m (for relative 
               efficiency)
-    maxiter - how many iterations of SVD? 
+    maxiter - how many iterations of power? 
+            - maxiter = -1 -> run until tolerance met
 
     RETURN: u - top left eigenvector approximation (n-dimensional)
             s - singular value (akin to eigenvalue)
             v - top right eigenvector approximation (m-dimensional)
     Adapted from section "4.4.2. Computing the top singular vector", found here:
     https://mmids-textbook.github.io/chap04_svd/04_power/roch-mmids-svd-power.html
+
+    For convergence checking, used this resource:
+    https://www.geeksforgeeks.org/python/power-method-determine-largest-eigenvalue-and-eigenvector-in-python/ 
     """
     x = v0.copy()
     B = A.T @ A 
+
+    # Normalize initial vector (good practice)
+    x = x / np.linalg.norm(x)
+
+    # top singular value is None for first iteration
+    s_prev = None
+
+    # Initialize v and s
+    v = None
+    s = None
+
     for _ in range(maxiter):
         x = B @ x
-    v = x / norm(x)
-    s = norm(A @ v)
+
+        # compute top left 
+        v = x / norm(x)
+
+        # top singular value
+        s = norm(A @ v)
+
+        # Check convergence
+        if s_prev is not None and abs(s - s_prev) < tolerance:
+            # print(i)
+            break
+        s_prev = s
+
     u = A @ v / s
     return u, s, v
