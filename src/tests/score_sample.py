cdb8f2b43355dcaeea6ed2a8c9668ee345f55c55
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-26 15:53:42 -0600

    implemented col_sample_inc_p (untested)

--- src/tests/score_sample.py
+++ src/tests/score_sample.py
@@ -12,6 +12,23 @@ from ..util import power as pwr
 from ..util import score as scr
 from ..util import scikit_jl as jl
 
+def get_reduce_funct(type):
+    """ Get the reduction function based on the type of reduction
+    
+    Args:
+        type: the type of reduction to do (string representation)
+
+    Return: the reduction function"""
+    match type.lower():
+        case "simple":
+            # simple random sampling of columns
+            return sub.select_d_random_columns
+        case "1-norm":
+            # random sampling of columns, with weight based on 1-norm of column
+            return  sub.one_norm_select
+        case _:
+            raise TypeError(f"Invalid sampling type: {type}")
+
 def col_sample(A, u_0, s_star, max_iter, tol, seed, d, type):
     """ Test of random col-sampling 
 
@@ -32,15 +49,8 @@ def col_sample(A, u_0, s_star, max_iter, tol, seed, d, type):
 
     A_reduced = None
 
-    match type.lower():
-        case "simple":
-            # simple random sampling of columns
-            A_reduced = sub.select_d_random_columns(A, d, seed)
-        case "1-norm":
-            # random sampling of columns, with weight based on 1-norm of column
-            A_reduced = sub.one_norm_select(A, d, seed)
-        case _:
-            raise TypeError(f"Invalid sampling type: {type}")
+    reduce = get_reduce_funct(type)
+    A_reduced = reduce(A=A, d=d, seed=seed)
 
     xs, ys, _ = baseline(A=A,
                          A_tilde=A_reduced,
@@ -85,6 +95,7 @@ def col_sample_p(A, u_0, s_star, max_iter, tol, seed, p, type):
                              )
 
     return xs, ys, f"{100 - p}% {lbl}"
+
 def reduce_A(A, p, seed, type):
     """ Reduce A based on percent reduction and reduction function
     
@@ -96,20 +107,10 @@ def reduce_A(A, p, seed, type):
     
     Return: the reduced A
     """
-    # Get reduction function
-    match type.lower():
-        case "simple":
-            # simple random sampling of columns
-            reduce = sub.select_d_random_columns
-        case "1-norm":
-            # random sampling of columns, with weight based on 1-norm of column
-            reduce =  sub.one_norm_select
-        case _:
-            raise TypeError(f"Invalid sampling type: {type}")
-        
     _, n = A.shape
 
     d = jl.percent_reduce(n=n, p=p)
+    reduce = get_reduce_funct(type)
     return reduce(A, d, seed)
 
 def col_sample_inc_p(A, u_0, s_star, max_iter, tol, seed, p0, type, step, inc_funct):
@@ -138,6 +139,7 @@ def col_sample_inc_p(A, u_0, s_star, max_iter, tol, seed, p0, type, step, inc_fu
     # Reduce A initially
     p = p0
     
+    A_tilde = reduce_A(A, p, seed, type)
 
     s_curr =  pwr.s_from_u(A, u_0)
     v =  pwr.v_from_u(A_tilde, u_0)
@@ -157,14 +159,14 @@ def col_sample_inc_p(A, u_0, s_star, max_iter, tol, seed, p0, type, step, inc_fu
             p = inc_funct(p)
 
             # Reduce A again with new reduction percentage
-            d = jl.percent_reduce(n=n, p=p)
-            A_tilde = reduce(A, d, seed)
+            A_tilde = reduce_A(A, p, seed, type)
         
         # NOTE: using scikit-learn -> top left eig (u) is of significance
-        u, _, v = pwr.topsing(v0=v,
-                              A=A_tilde, 
-                              maxiter=1,
-                              ) #TODO: maybe use this s for convergence? 
+        u, _, v = pwr.topsing(
+            v0=v,
+            A=A_tilde, 
+            maxiter=1,
+        ) #TODO: maybe use this s for convergence? 
         
         # Get score of current approximation of top left eigenvector
         s_curr =  pwr.s_from_u(A, u)
@@ -184,7 +186,4 @@ def col_sample_inc_p(A, u_0, s_star, max_iter, tol, seed, p0, type, step, inc_fu
         # Save current score as previous
         s_prev = s_curr
 
-    return xs, ys, f"baseline {A.shape}"
-
-
-    return xs, ys, f"{100 - p}%, increasing every {step} iterations: {lbl}"
+    return xs, ys, f"{100 - p}%, increasing every {step} iterations"
