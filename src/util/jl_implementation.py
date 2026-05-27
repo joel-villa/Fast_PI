53032bdca93947d765ed1a97c6197726c0f446c7
Joel Villarreal II <joel.villa2329@gmail.com>
2026-05-27 14:01:20 -0600

    jl-gauss implementation (untested)

--- src/util/jl_implementation.py
+++ src/util/jl_implementation.py
@@ -54,3 +54,42 @@ def jl_simple(X, d, seed, eps=0.9):
     X_reduced = X @ jl_matrix(n=n, d=d, seed=seed)
 
     return X_reduced
+
+def jl_gauss_mat(n, d, seed):
+    """ Randomly generate a JL matrix of dimensions (nxd)
+
+    Args: 
+        n: first dimension of reduction matrix
+        d: second dimension of reduction matrix
+        seed: for reproducible randomness
+
+    Return: JL reduction matrix
+
+    See Section 4.1 of:
+    https://www.cs.unm.edu/~saia/classes/506-s24/lec/HighDim+JLProjection.pdf
+    """
+    #TODO: untested
+    rng = np.random.default_rng(seed)
+    G = rng.normal(0, 1, size=(n, d))
+    return G * (1 / math.sqrt(n))
+
+# def jl_gauss(X, d, seed, eps=0.9):
+#     """Reduce dimensions of A, via simple jl projection
+
+#     Args:
+#         X: original matrix (mxn)
+#         d: desired dimension
+#         seed: for repeatable randomness
+#         eps: allowable error
+
+#     RETURN: reduced X (nxd), i.e. less columns
+#     """
+    
+#     util.check_valid_dimensions(X)
+#     util.check_safe(X, d, eps)
+
+#     _, n = X.shape
+
+#     X_reduced = X @ jl_gauss_mat(n=n, d=d, seed=seed)
+
+#     return X_reduced
