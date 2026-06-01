c806ed99c123713fe1f070a5e37d3a0f7b3901ec
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-01 10:45:49 -0600

    updated count_mults() calls to take in a v0

--- src/tests/subset_power.py
+++ src/tests/subset_power.py
@@ -6,22 +6,25 @@ import numpy as np
 from ..util import power as pwr
 from ..util import eig_functs as eig
 from ..util import scikit_jl as jl
-from ..util.subset import select_d_random_columns
+from ..util import subset as sub
 
 #TODO: something similar to Chan's (randomly pick larger and larger sample-sizes)
 
-def subset_pow(A, u_0, u_star, num_iter, seed, d):
-    """
-    A - the matrix (nxm)
-    u_0 - an initial guess for the top left eigenvector of A
-    u_star - the actual top left eigenvector of A
-    num_iter - the number of iterations of power to do
-    seed - for repeatable tests
-    d - reduction size: A_reduced is (nxd)
+def subset_pow(A, u_0, u_star, num_iter, seed, d, type):
+    """Measure convergence of Power with some random subset of the columns of A
 
-    RETURN: xs - a list of iterations [0, 1, 2, ..., num_iter - 1]
-            ys - the list of residuals per iteration
-    Measure convergence of Power with some random subset of the columns of A
+    Args: 
+        A: the matrix (nxm)
+        u_0: an initial guess for the top left eigenvector of A
+        u_star: the actual top left eigenvector of A
+        num_iter: the number of iterations of power to do
+        seed: for repeatable tests
+        d: reduction size: A_reduced is (nxd)
+        type: the type of reduction to do
+
+    Return: 
+        xs: a list of iterations [0, 1, 2, ..., num_iter - 1]
+        ys: the list of residuals per iteration
     """
 
     xs = np.zeros(num_iter)
@@ -30,7 +33,9 @@ def subset_pow(A, u_0, u_star, num_iter, seed, d):
     ys[0] = eig.euclidean_dist(u_0, u_star)
     xs[0] = 0
 
-    A_reduced = select_d_random_columns(A, d, seed)
+    sub.reduce_A(A=A, d=d, seed=seed, type="col_random", gamma=None)
+
+    A_reduced = sub.select_d_random_columns(A, d, seed)
 
     v =  pwr.v_from_u(A_reduced, u_0)
 
@@ -45,27 +50,30 @@ def subset_pow(A, u_0, u_star, num_iter, seed, d):
         ys[i] = euc_dist
         xs[i] = i
     
-    num_mults = pwr.count_mults(A=A_reduced, maxiter=num_iter - 1)
+    num_mults = pwr.count_mults(v0=v, A=A_reduced, maxiter=num_iter - 1)
 
     return xs, ys, f"column subset {A_reduced.shape}; {num_mults:,} mults"
 
-def percent_subset_pow(A, u_0, u_star, num_iter, seed, p):
-    """
-    A - the matrix (nxm)
-    u_0 - an initial guess for the top left eigenvector of A
-    u_star - the actual top left eigenvector of A
-    num_iter - the number of iterations of power to do
-    seed - for repeatable tests
-    p - reduction percentage: A_reduced is (nx(1-p)*m)
+def percent_subset_pow(A, u_0, u_star, num_iter, seed, p, type):
+    """Measure convergence of Power with some random subset of the columns of A
 
-    RETURN: xs - a list of iterations [0, 1, 2, ..., num_iter - 1]
-            ys - the list of residuals per iteration
-    Measure convergence of power with some random subset of the columns of A
+    Args: 
+        A: the matrix (nxm)
+        u_0: an initial guess for the top left eigenvector of A
+        u_star: the actual top left eigenvector of A
+        num_iter: the number of iterations of power to do
+        seed: for repeatable tests
+        p: reduction percentage
+        type: the type of reduction to do
+
+    Return: 
+        xs: a list of iterations [0, 1, 2, ..., num_iter - 1]
+        ys: the list of residuals per iteration
     """
 
     d = jl.percent_reduce(A.shape[1], p)
 
-    xs, ys, label = subset_pow(A, u_0, u_star, num_iter, seed, d)
+    xs, ys, label = subset_pow(A, u_0, u_star, num_iter, seed, d, type)
 
     return xs, ys, f"{p}% {label}"
 
@@ -90,7 +98,7 @@ def subset_pow_swap(A, u_0, u_star, num_iter, seed, d, step_size):
     ys[0] = eig.euclidean_dist(u_0, u_star)
     xs[0] = 0
 
-    A_reduced = select_d_random_columns(A, d, seed)
+    A_reduced = sub.select_d_random_columns(A, d, seed)
 
     v =  pwr.v_from_u(A_reduced, u_0)
 
@@ -98,7 +106,7 @@ def subset_pow_swap(A, u_0, u_star, num_iter, seed, d, step_size):
         # NOTE: using scikit-learn -> top left eig (u) is of significance
         if (i % step_size == 0):
             # Randomly regenerate A
-            reduced_A = select_d_random_columns(A, d=d, seed=seed*i)
+            reduced_A = sub.select_d_random_columns(A, d=d, seed=seed*i)
             v = pwr.v_from_u(reduced_A, u)
             # TODO: not currently counting number of mults in v_from_u() call
 
@@ -110,7 +118,7 @@ def subset_pow_swap(A, u_0, u_star, num_iter, seed, d, step_size):
         ys[i] = euc_dist
         xs[i] = i
 
-    num_mults = pwr.count_mults(A=A_reduced, maxiter=num_iter - 1)
+    num_mults = pwr.count_mults(v0=v, A=A_reduced, maxiter=num_iter - 1)
 
     return xs, ys, f"column subset {A_reduced.shape} swapping every {step_size}; {num_mults:,} mults"
 
