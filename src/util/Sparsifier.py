c9f40117968fec5df3d36074742040b487a62ba2
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-02 12:51:26 -0600

    sparssifier based on entry weights

---
+++ src/util/Sparsifier.py
@@ -0,0 +1,134 @@
+"""
+Based on Thm 1.5 in: https://doi.org/10.1145/1219092.1219097
+
+Sparsifying matrix based on its entries, with higher probability of keeping
+entries with higher magnitude
+"""
+
+import numpy as np
+
+class Sparsifier():
+    """
+    A direct implementation of Thm 1.5
+    """
+    def __init__(self, seed):
+        self.seed(seed)
+
+    def seed(self, seed):
+        """ Set the seed of the random number generator
+        """
+        self.rng = np.random.default_rng(seed=seed)
+
+    def p_valid(self, p):
+        """ Check if p is valid for a matrix with given dimensions
+        Args:
+            p: sparsification probability
+
+        Return: True if valid, False if not
+        """
+
+        if p <= 0 or p > 1:
+            # p out of bounds
+            # print(f"INVALID p, {p} is not in (0, 1]")
+            return False
+        
+        return True
+    
+    def prob_error(self, n):
+        """ Get the probability of error associated with the sparsification
+        process
+
+        Thm 1.5 (1)
+            probability of error is: 1 - exp(-19(log n)^4)
+
+        Args:
+            n: the number of nonzeroes in the matrix being sparsified? TODO: confirm
+
+        Return: the probability of error
+        """
+        return 1 - np.exp(-19 * (np.log(n))**4)
+    
+    def exp_nnz(self, A, p, m, n, b):
+        """ Get the expected number of nonzeroes in the sparsified matrix
+
+        Thm 1.5 (2):
+            (pmn) x Avg[(A_ij /b)^2] + m(8 log n)^4
+
+        Args:
+            A: the matrix being sparsified
+            p: the sparsification factor
+            m: the number of rows in the matrix
+            n: the number of columns in the matrix
+            b: the maximum absolute value of an entry in the matrix
+
+        Return: the expected number of nonzeroes in the sparsified matrix
+        """
+        
+        avg_sqr = np.mean((A.data / b) ** 2) 
+        return p * m * n * avg_sqr + m * (8 * np.log(n)) ** 4
+
+    def sparse_entry(self, x, p):
+        """
+        Scale value or make value zero
+
+        Args: 
+            x: the value to either scale up or make zero
+            p: the probability of keeping the entry
+
+        RETURN: x scaled up by a factor of s, zero otw
+        """
+        r = self.rng.random() # r in range [0.0, 1.0)
+
+        if r < p:
+            # With probability p scale the entry up
+            return x / p
+        else: 
+            # With probability 1 - p make the entry zero
+            return 0.0
+        
+
+    def sparsify(self, A, p):
+        ''' Implementation of Thm 1.5 in: 
+        https://doi.org/10.1145/1219092.1219097
+
+        Increasing p => make more sparse 
+
+        Sparsify a matrix A given some value p
+        
+        Args: 
+            A: the matrix to sparsify
+            p: factor of sparsification
+
+        Return: NONE, sparsifies A in place
+        '''
+
+        # b is the maximum absolute value of an entry in A
+        b = abs(A).max()
+
+        # Number of nonzeroes in A
+        nnz = A.nnz
+
+        # Approx dimensions of a dense matrix with nnz nonzeroes
+        sqrt_nnz = np.sqrt(nnz)
+        #TODO is this correct? or should it just be nxm?
+        n_approx = np.ceil(sqrt_nnz) 
+        m_approx = np.floor(sqrt_nnz)
+
+        print(f"probability of error: {self.prob_error(n=n_approx)}")
+
+        for i, datum in enumerate(A.data):
+            importance = p * (datum / b) ** 2
+            prob_sparse = max(importance,  np.sqrt(importance * (8 * np.log(n_approx)) ** 4 / 4))
+            A.data[i] = self.sparse_entry(x=datum, p=prob_sparse)
+
+        A.eliminate_zeros()
+        
+        exp_nnz = self.exp_nnz(A=A, p=p, m=m_approx - 1, n=n_approx, b=b)
+        act_nnz = A.nnz
+
+        if (abs(act_nnz - exp_nnz) > 0.025 * exp_nnz):
+            print(f"WARNING: number of nonzeroes in sparsified matrix is more than 2.5% off expected number of nonzeroes")
+
+        return None
+    
+
