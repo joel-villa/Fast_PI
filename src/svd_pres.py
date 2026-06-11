84ffbd32ecd0dc07ff1b20b8591c999c22fb51d2
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-11 09:13:56 -0600

    svd preservation of sparsififcation technique

--- src/svd_pres.py
+++ src/svd_pres.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from scipy.sparse.linalg import svds
 
-from .tests.svd_pres import subset_p
+from .tests import svd_pres as tst
 
 from Sparsification_Research.src.Plotter import Plotter
 from Sparsification_Research.src.SSGetter import SSGetter
@@ -27,8 +27,10 @@ def init(mat_name):
     return A
 
 if __name__ == '__main__':
+    ps = np.arange(0, 100, 5)
     seed = 10
     k = 5
+
     sub_types = (
         # "random", 
         # "1-norm", 
@@ -36,8 +38,14 @@ if __name__ == '__main__':
         "scale",
         # "nystrom",
     )
-    ps = np.arange(0, 100, 10)
-    gamma = 0.9
+    spr_types = (
+        # "Generic",
+        "SD",
+        # "CD",
+        # "MB", #TODO test this
+    )
+
+    gamma = 0.9 #For nystrom sampling
     
     mats = [
         "494_bus", # Invalid for mag-based sparsifification
@@ -76,7 +84,7 @@ if __name__ == '__main__':
 
         sub_i = 0
 
-        xs, ys = subset_p(
+        xs, ys = tst.subset_p(
             A=A, 
             u_0=u0, 
             u_star=u_star, 
@@ -89,6 +97,19 @@ if __name__ == '__main__':
 
         for i in range(ys.shape[1]):
             plotter.add_to_plot(xs, ys[:, i], label=f"k = {i + 1} ({sub_types[sub_i]})")
+
+        # xs, ys = tst.sparse_p(
+        #     A=A, 
+        #     u_0=u0, 
+        #     u_star=u_star, 
+        #     k=k, 
+        #     seed=seed, 
+        #     ps=ps, 
+        #     type=spr_types[sub_i], 
+        # )
+
+        # for i in range(ys.shape[1]):
+        #     plotter.add_to_plot(xs, ys[:, i], label=f"k = {i + 1} ({spr_types[sub_i]})")
 
         plotter.finish()
 
--- src/tests/svd_pres.py
+++ src/tests/svd_pres.py
@@ -8,7 +8,10 @@ Functions in this file have Return type as follows:
 import numpy as np
 
 from scipy.sparse.linalg import svds
+
 from ..util.subset import p_reduce_A
+from ..util.sparsify import p_sparse
+
 
 def eig_pres(A_tilde, u0, u_star, k, seed):
     """ Test the preservation of A's top-left eigenvectors
@@ -88,7 +91,44 @@ def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
             k=k,
             seed=seed,
         )
-        # print(f"type(ys_i): {type(ys_i)}, type(ys): {type(ys)}")
+        ys[i] = ys_i
+    
+    return ps, ys
+
+def sparse_p(A, u_0, u_star, k, seed, ps, type):
+    """ Preservation of top k eigenvectors of a sparsified version of A
+
+    Args: 
+        A: the original matrix
+        u_0: initial guess for top left singular vectors of A
+        u_star: The top-k left singular vectors of the original matrix
+        k: The number of singular vectors to compute
+        seed: for repeated randomness
+        ps: reduction percentages
+        type: string representation of sparsification type
+
+    Returns:
+        xs: an array of reduction percentages (ps)
+        ys: a 2d array of relative error in top k left singular vectors 
+            (each column represents a distinct k-value)
+    """
+    ys_shape = (ps.shape[0], k)
+    ys = np.zeros(ys_shape)
+
+    for i, p in enumerate(ps):
+        A_sparse = p_sparse(
+            A=A, 
+            p=p,
+            seed=seed,
+            type=type,
+        )
+        ys_i = eig_pres(
+            A_tilde=A_sparse,
+            u0=u_0,
+            u_star=u_star,
+            k=k,
+            seed=seed,
+        )
         ys[i] = ys_i
     
     return ps, ys
