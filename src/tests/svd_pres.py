4019d520c17e0f3baaa01dbc1b4a18437f6aa493
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-19 12:05:01 -0600

    eig_pres_right()

--- src/tests/svd_pres.py
+++ src/tests/svd_pres.py
@@ -13,7 +13,7 @@ from ..util.subset import p_reduce_A
 from ..util.sparsify import p_sparse
 
 
-def eig_pres(A_tilde, u0, u_star, k, seed):
+def eig_pres_left(A_tilde, u0, u_star, k, seed):
     """ Test the preservation of A's top-left eigenvectors
     
     Args:
@@ -56,6 +56,50 @@ def eig_pres(A_tilde, u0, u_star, k, seed):
     
     return relative_error
 
+
+def eig_pres_right(A_tilde, v0, v_star, k, seed):
+    """ Test the preservation of A's top-right eigenvectors
+    
+    Args:
+        A_tilde: The modified version of A
+        v0: Initial guess for top right eigenvectors
+        v_star: The top-k right singular vectors of the original matrix
+        k: The number of singular vectors to compute
+        seed: for repeated randomness
+    Return: 
+        2-norm difference between A_tilde's top-right singular vector's and A's
+        top-right singular vectors
+    """
+
+    u, s, vh = svds(
+        A=A_tilde,
+        k=k,
+        # maxiter=256, #TODO: do we need this? 
+        rng=seed,
+    )
+
+    # Difference from actual
+    diff1 = vh - v_star
+    diff2 = vh + v_star
+
+    # 2-norm of difference (right -> rows)
+    norm1  = np.linalg.norm( diff1, ord=2, axis=1)
+    norm2  = np.linalg.norm( diff2, ord=2, axis=1)
+    norm_v = np.linalg.norm(v_star, ord=2, axis=1)
+
+    # Take minimum 2-norm difference per column
+    eig_perturbance = np.minimum(norm1, norm2)
+
+    # Error relative to the magnitude of the solution
+    relative_error = eig_perturbance / norm_v
+
+    relative_error = relative_error.flatten()
+
+    if relative_error.shape[0] != k:
+        raise ValueError(f"{relative_error.shape} != {k} = k")
+    
+    return relative_error
+
 def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
     """ Test of random col sampling based on percent of reduction
 
@@ -84,7 +128,7 @@ def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
             type=type,
             gamma=gamma,
         )
-        ys_i = eig_pres(
+        ys_i = eig_pres_left(
             A_tilde=A_reduced,
             u0=u_0,
             u_star=u_star,
@@ -122,7 +166,7 @@ def sparse_p(A, u_0, u_star, k, seed, ps, type):
             seed=seed,
             type=type,
         )
-        ys_i = eig_pres(
+        ys_i = eig_pres_left(
             A_tilde=A_sparse,
             u0=u_0,
             u_star=u_star,
