fb72ab72cfb8887cc4064155da337207dd126999
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-19 12:28:10 -0600

    added pi_inc_pow()

--- src/tests/svd_pres.py
+++ src/tests/svd_pres.py
@@ -4,6 +4,7 @@ A file containing tests for singular vector preservation
 Functions in this file have Return type as follows:
     xs: an array of reduction percentages (ps)
     ys: a 2d array of relative error in top k left singular vectors
+    lbl: a string representation of this test
 """
 import numpy as np
 
@@ -11,6 +12,7 @@ from scipy.sparse.linalg import svds
 
 from ..util.subset import p_reduce_A
 from ..util.sparsify import p_sparse
+from ..util.proven import get_A_tilde
 
 
 def eig_pres_left(A_tilde, u0, u_star, k, seed):
@@ -76,6 +78,7 @@ def eig_pres_right(A_tilde, v0, v_star, k, seed):
         k=k,
         # maxiter=256, #TODO: do we need this? 
         rng=seed,
+        v0=v0,
     )
 
     # Difference from actual
@@ -100,6 +103,45 @@ def eig_pres_right(A_tilde, v0, v_star, k, seed):
     
     return relative_error
 
+def pi_inc_pow(A, v0, v_star, k, seed, pows, gen_type, sf_kwargs):
+    """ Test of random col sampling based on percent of reduction
+
+    Args:
+        A: the matrix to do power iteration on
+        v0: initial guess for top right singular vectors
+        v_star: The top-k right singular vectors of the original matrix
+        k: The number of singular vectors to compute
+        seed: for repeated randomness
+        ps: reduction percentages
+        gen_type: string representation of reduction type
+    Return:
+        xs: an array of reduction percentages (ps)
+        ys: a 2d array of relative error in top k left singular vectors 
+            (each column represents a distinct k-value)
+        lbl: a string representation of this test
+    """
+    ys_shape = (pows.shape[0], k)
+    ys = np.zeros(ys_shape)
+
+    for i, pow in enumerate(pows):
+        pow_kwargs = sf_kwargs | {"power": pow}
+        A_tilde = get_A_tilde(
+            A=A,
+            gen_type=gen_type,
+            sf_kwargs=pow_kwargs,
+            seed=seed
+        )
+        ys_i = eig_pres_right(
+            A_tilde=A_tilde,
+            v0=v0,
+            v_star=v_star,
+            k=k,
+            seed=seed,
+        )
+        ys[i] = ys_i
+    
+    return pows, ys, gen_type
+
 def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
     """ Test of random col sampling based on percent of reduction
 
@@ -116,6 +158,7 @@ def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
         xs: an array of reduction percentages (ps)
         ys: a 2d array of relative error in top k left singular vectors 
             (each column represents a distinct k-value)
+        lbl: a string representation of this test
     """
     ys_shape = (ps.shape[0], k)
     ys = np.zeros(ys_shape)
@@ -137,7 +180,7 @@ def subset_p(A, u_0, u_star, k, seed, ps, type, gamma):
         )
         ys[i] = ys_i
     
-    return ps, ys
+    return ps, ys, type
 
 def sparse_p(A, u_0, u_star, k, seed, ps, type):
     """ Preservation of top k eigenvectors of a sparsified version of A
@@ -155,6 +198,7 @@ def sparse_p(A, u_0, u_star, k, seed, ps, type):
         xs: an array of reduction percentages (ps)
         ys: a 2d array of relative error in top k left singular vectors 
             (each column represents a distinct k-value)
+        lbl: a string representation of this test
     """
     ys_shape = (ps.shape[0], k)
     ys = np.zeros(ys_shape)
@@ -175,4 +219,4 @@ def sparse_p(A, u_0, u_star, k, seed, ps, type):
         )
         ys[i] = ys_i
     
-    return ps, ys
+    return ps, ys, type
