db2c7794fa98b0cde89da8e3f0220248205b1ab5
Joel Villarreal II <joel.villa2329@gmail.com>
2026-06-19 11:34:25 -0600

    norm_max_based() now has raising the scales to some power (input by user)

--- src/util/s_funcs.py
+++ src/util/s_funcs.py
@@ -30,19 +30,32 @@ def uniform(A, s):
 
     return np.full(A.shape[0], s)
 
-def norm_max_based(A, norm_ord):
+def norm_max_based(A, norm_ord, power):
     """ Probability of selecting i'th column is dependent on the norm of A, 
     se
     
     Args:
         A: a CSR sparse matrix (mxn)
+        norm_ord: what order norm to use
+        power: to what power to raise the scales
+            increasing power -> scales closer to zero -> less rows chosen -> higher variance
+            decreasing power -> scales closer to one -> more rows chosen -> lower variance
     Return: a numpy array of length m, which gives the probability of selecting
             any of the m rows of A, indendently
     """
+    if power < 0:
+        raise ValueError(f"power must be nonegative, {power} invalid")
+    if power == 0:
+        # All scales are one -> return a one-filled array of length m
+        return np.full(A.shape[0], 1)
+    
     row_norms = norm(A, ord=norm_ord, axis=1)
     max_row_norm = row_norms.max()
 
     # Divide all by the maximum row_norm
     scales = row_norms / max_row_norm
+
+    # Raise the scales to the power
+    scales = scales ** power
 
     return scales
