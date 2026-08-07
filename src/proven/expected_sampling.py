4c8d0494fff2b205159b38d6df3aa94669e9a632
Joel Villarreal II <joel.villa2329@gmail.com>
2026-08-07 13:37:14 -0600

    implemented expected_sampling_ub (untested)

--- src/proven/expected_sampling.py
+++ src/proven/expected_sampling.py
@@ -1,12 +1,15 @@
 """A file for generating the expected sampling of a matrix A, given the sampling
 scheme which was proven to be theoretically perfomant"""
 
-def expected_sampling(
+from .util.meta_data import get_n_c_k
+from ..util.constants import THIRTY_TWO_BIT_PRECISION
+
+def expected_sampling_ub(
     matrix: str,
 ) -> float:
-    """ Get the expected sampling of the matrix, under the assumption that the 
-    sampling scheme is to pick the i'th row with probability pi, and to scale 
-    the i'th row by 1/sqrt(pi).
+    """ Get an upper bound on the expected sampling of the matrix, under the 
+    assumption that the sampling scheme is to pick the i'th row with probability
+    pi, and to scale the i'th row by 1/sqrt(pi).
 
     Args:
         matrix (str): The name of a Suite-Sparse matrix
@@ -14,4 +17,10 @@ def expected_sampling(
     Returns:
         float: expected rows kept of that matrix
     """
+    n, c, k = get_n_c_k(matrix_name=matrix)
+
+    if (abs(k + 1) <= THIRTY_TWO_BIT_PRECISION):
+        # k = -1 check
+        raise ValueError(f"For {matrix}, k = {k}")
     
+    return c * (1 + (((n ** (k + 1)) - 1) / (k + 1)))
