82712368ae99ed03ec973bc9ba1b8fa40a05c65e
Joel Villarreal II <joel.villa2329@gmail.com>
2026-08-11 14:06:00 -0600

    moved constants here + new variance proxy metadatum

--- src/proven/util/comp_data.py
+++ src/proven/util/comp_data.py
@@ -1,12 +1,34 @@
 """A file for computing the metadata of theoretical significance of a given 
 sparse matrix"""
 
+import math
+
+import numpy as np
+
 import scipy
 from scipy.sparse.linalg import norm
 
 from ..preprocess import preprocess
 
 from ...util.row_norms import get_sorted_row_norms, get_power_law_coefficients
+
+N_STRING:str = "n"
+C_STRING:str = "c"
+K_STRING:str = "k"
+KAPPA_STRING:str = "kappa"
+NORM_STRING:str = "op_norm"
+VAR_STRING:str = "var_proxy"
+SCALE_STRING:str = "scale_factor"
+
+META_DATA:set[str] = { 
+    N_STRING,
+    C_STRING, 
+    K_STRING, 
+    KAPPA_STRING, 
+    NORM_STRING,
+    VAR_STRING,
+    SCALE_STRING,
+}
 
 
 def get_c_k(mat_name:str) -> tuple[float, float]:
@@ -48,6 +70,45 @@ def get_op_norm(A:scipy.sparse) -> float:
     """
     return norm(A, ord=2)
 
+def get_var_proxy(A:scipy.sparse) -> float:
+    """The variance proxy from Matrix Bernstein's
+
+    Args:
+        A (scipy.sparse): The matrix in question
+
+    Returns:
+        float: || sum_{i=1}^n (||a_i|| - ||a_i||^2)a_i^Ta_i||
+    """
+
+    sum_mat = np.zeros(shape=A.shape)
+
+    for i in range(A.shape[0]):
+        # Loop through the rows
+        row_i = A[i, :] 
+
+        # Compute row magnitudes
+        i_vals = row_i.data
+        norm_squared = np.sum(i_vals * i_vals) # sum of the square of the values
+        norm_i = math.sqrt(norm_squared)
+
+        if norm_i > 1:
+            # All rows should have row magnitudes less than or equal to 1
+            raise ValueError(f"i'th row magnitude is {norm_i}, {norm_i} > 1")
+
+        scalar_factor = norm_i * (1 - norm_i) 
+        outer_prod = row_i.T @ row_i
+
+        if (outer_prod.shape[0] <= 1):
+            raise ValueError(f"YOU FUCKED IT (TODO: delete this)")
+
+        sum_mat += scalar_factor * outer_prod
+
+    var_proxy = np.linalg.norm(sum_mat, ord=2)
+
+    return float(var_proxy)
+
+        
+
 def get_metadata(
         mat_name: str,
 ) -> dict[str, int | float]:
@@ -63,19 +124,23 @@ def get_metadata(
             "c": y-intercept of power-law distribution
             "k": slope of power-law distribution
             "kappa": -3k - 1 (always positive)
-            "op-norm": ||A||_2
+            "op_norm": ||A||_2
+            "var_proxy": variance proxy from matrix bernsteins
+            "scale_factor": how much the matrix is scaled to get ||a_i|| <= 1
     """
     A, scale_factor = preprocess(mat_name)
     n = A.shape[0]
     c, k = get_c_k(mat_name)
     kappa = get_kappa(k)
     op_norm = get_op_norm(A)
+    var_proxy = get_var_proxy(A)
 
     return {
-        "n": n,
-        "c": c,
-        "k": k,
-        "kappa": kappa,
-        "op_norm": op_norm,
-        "scale_factor": scale_factor,
+        N_STRING: n,
+        C_STRING: c,
+        K_STRING: k,
+        KAPPA_STRING: kappa,
+        NORM_STRING: op_norm,
+        VAR_STRING: var_proxy,
+        SCALE_STRING: scale_factor,
     }
