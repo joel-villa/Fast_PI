eaca15ba232525ed8641206e61793ee1aa501741
Joel Villarreal II <joel.villa2329@gmail.com>
2026-09-08 14:36:22 -0600

    a file for the matrix bernstein results

---
+++ src/proven/mat_bernstein.py
@@ -0,0 +1,5 @@
+"""Ooh-wee, finally some good results, how exciting:
+
+With probability at least 1 - exp(ln(2n) - N * ||Aw||^2 * epsilon^2 / 4):
+    ||A~w|| is in [(1 - epsilon) * ||Aw||, ||Aw||]
+"""
