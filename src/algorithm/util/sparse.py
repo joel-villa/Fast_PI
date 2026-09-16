5004561d41d238cdf265dc9a61ae85e5f405678f
Joel Villarreal II <joel.villa2329@gmail.com>
2026-09-16 12:53:44 -0600

    this file proved useless (random mats will NEVER be equal, duh)

--- src/algorithm/util/sparse.py
+++ src/algorithm/util/sparse.py
@@ -2,15 +2,31 @@
 import scipy
 import numpy as np
 
+#TODO: delete this file
+def check_equal_dense(
+        A:scipy.sparse.sparray,
+        B:scipy.sparse.sparray,
+) -> bool:
+    """Grossly checking equality by converting to dense
+
+    Args:
+        A (scipy.sparse.sparray): _description_
+        B (scipy.sparse.sparray): _description_
+
+    Returns:
+        bool: _description_
+    """
+    return np.array_equal(A.todense(), B.todense())
+
 def check_equal(
-        A:scipy.sparse,
-        B:scipy.sparse,
+        A:scipy.sparse.sparray,
+        B:scipy.sparse.sparray,
 ) -> bool:
     """Check if two scipy sparse matrices are equivalent
 
     Args:
-        A (scipy.sparse): One of the sparse matrices
-        B (scipy.sparse): Another sparse matrix
+        A (scipy.sparse.sparray): One of the sparse matrices
+        B (scipy.sparse.sparray): Another sparse matrix
 
     Returns:
         bool: True if A == B, False otw
@@ -30,4 +46,4 @@ if __name__ == '__main__':
     A = scipy.sparse.csr_array(([1, 2], [0, 1], [0, 0, 2]))
     B = scipy.sparse.csr_array(([2, 1], [1, 0], [0, 0, 2]))
     print(check_equal(A, B))
-    print(np.array_equal(A.todense(), B.todense()))
+    print(check_equal_dense(A, B))
